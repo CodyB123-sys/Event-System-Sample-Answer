3a755cc190d24352c3126e80fa73b6c492b7ce3c
Cody Beinert <codybeinert123@gmail.com>
2021-06-15 16:04:01 -0400

    add everything again for real

---
+++ Attempting Raycasts in 2d/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject start;
+    public float range = 100f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    //Everything below is meant to shoot a raycast to the right when the player presses right, and then debug what the raycast hit.
+    //The raycast only shoots for a split second after every few button presses, and each time it states that it hits the player.
+    //If you're able to tell me what is wrong, that would be very much appreciated.
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ShootRight();
+        }
+    }
+
+    void ShootRight()
+    {
+        Debug.DrawRay(start.transform.position, start.transform.TransformDirection(Vector2.right) * 100f, Color.red);
+
+        RaycastHit2D hit = Physics2D.Raycast(start.transform.position, start.transform.TransformDirection(Vector2.right), 100f);
+
+        if (hit)
+        {
+            Debug.Log(hit.collider.name);
+        }
+
+    }
+}
