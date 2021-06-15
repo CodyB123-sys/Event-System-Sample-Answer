c405f70aaa70d886ea02744c2a48b321712ef4e9
Cody Beinert <codybeinert123@gmail.com>
2021-06-15 15:56:19 -0400

    Insert everything that was added

---
+++ New Unity Project/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject options;
+    
+
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    //load the "game"
+    public void PlayGame() 
+    {
+        MainMenuExit();
+        Invoke("StartGame", 0.3f);
+    }
+
+    //quit the game
+    public void QuitGame() 
+    {
+        MainMenuExit();
+        Invoke("CloseGame", 0.3f);
+    }
+
+    public void OptionsClick()
+    {
+        MainMenuExit();
+        options.SetActive(true);
+        options.SendMessage("OptionsMenuEnter");
+    }
+
+    void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    void StartGame() 
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    void MainMenuEnter() 
+    {
+        animator.Play("MainMenuEnter");
+    }
+
+    void MainMenuExit()
+    {
+        animator.Play("MainMenuExit");
+    }
+
+    void OptionsMenuEnter() 
+    {
+        options.SendMessage("OptionsMenuEnter");
+    }
+}
---
+++ New Unity Project/Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsScript : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void OptionsMenuEnter() 
+    {
+        animator.Play("OptionsMenuEnter");
+    }
+
+    public void OptionsMenuExit()
+    {
+        animator.Play("OptionsMenuExit");
+    }
+
+    public void BackClick()
+    {
+        animator.Play("OptionsMenuExit");
+        menu.SetActive(true);
+        menu.SendMessage("MainMenuEnter");
+    }
+
+    public void MainMenuEnter()
+    {
+        menu.SendMessage("MainMenuEnter");
+    }
+}
